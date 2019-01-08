349818def74dc8c2f05fb97cbe3b719aa0282d04
朱慧君 <huijun.zhu@ymm56.com>
2019-01-08 13:41:24 +0800

    change labels name

--- prometheus/client.go
+++ prometheus/client.go
@@ -100,6 +100,7 @@ func (v *QueryRangeResponseValue) Time() time.Time {
 func GetPromContainerCpuUsage(pod_name string,prom_url string,sample int64) (timestamp string,value string,err error){
 	//query_str := "100 * (1 - avg by(instance_type, availability_zone)(irate(node_cpu{mode='idle'}[5m])))"
 	query_str := "sum by (container_name) (rate(container_cpu_usage_seconds_total{job='kubelet', image!='',container_name!='POD',pod_name='" + pod_name + "'}[1m]))"
+	fmt.Println(query_str)
 
 	client,err := NewClient(prom_url)
 	if err != nil {
